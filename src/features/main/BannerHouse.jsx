5cb2bffd3b965a817cd9a1aea83b096d5f43ee0d
Phong Nguyen Hong <intern.nhphong1@tma.com.vn>
2022-04-06 16:25:28 +0700

    06/04

--- src/features/main/BannerHouse.jsx
+++ src/features/main/BannerHouse.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 import './index.scss';
 import './Banner.scss';
 import images from './images';
-
+import ReactCaroussel from 'react-caroussel';
+import "react-caroussel/dist/index.css";
 class BannerHouse extends React.Component{
   
-  render(){
-    // var myIndex = 0;
-    // carousel();
-    // function carousel() {
-    //   var i;
-    //   var x = document.getElementsByClassName("MySlides");
-    //   for (i = 0; i < x.length; i++) {
-    //     x[i].style.display = "none";  
-    //   }
-    //   myIndex++;
-    //   if (myIndex > x.length) {myIndex = 1}    
-    //   x[myIndex-1].style.display = "block";  
-    //   setTimeout(carousel, 1000); // Change image every 2 seconds
-    // }
-        return (
-          <div className='containBanner'>
-           
-            <div>
-                  <img class="MySlides" src={images.tro1} style={{width:"100%",height:400}} />
-                   <img class="MySlides" src={images.tro2} style={{width:"100%",height:400}}/>
-                  <img class="MySlides" src={images.tro3} style={{width:"100%",height:400}}/>
-                  <img class="MySlides" src={images.tro4} style={{width:"100%",height:400}}/>
-                  <img class="MySlides" src={images.tro5} style={{width:"100%",height:400}}/>
-                  <img class="MySlides" src={images.tro6} style={{width:"100%",height:400}}/>
-            </div>
+            
+        render(){
           
-             
-          </div>
-        );
+            return (
+            <div className='containBanner'>
+            <div>
+            <ReactCaroussel className ='carsousel'
+               slidesToShow={3}
+              slidesToScroll={3}
+                infinite={true}
+               autoplay={false}
+              speed={2}    // 2s: speed of autoplay
+              display={{ 
+                arrows: true, 
+                dots: true 
+                }}>
+                      <div className='di1'>
+                      <img class="mySlides" src={images.tro1} style={{width:"auto",height:"auto"}}/>
+                      </div>
+                      <div className='di1'>2</div>
+                      <div className='di1'>3</div>
+                      <div className='di1'> 4</div>
+                      <div className='di1'>5</div>
+                      <div className='di1'>6</div>
+        </ReactCaroussel>
+            </div>
+            </div>
+            );
     }
 }
 
